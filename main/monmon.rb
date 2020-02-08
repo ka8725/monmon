ed847854466d68f81a9d3d57a3c52248fd2570dd
Sergey <koleshkoser@gmail.com>
2020-02-08 15:52:08 +0300

    optimized adding financial sources

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,23 +1,21 @@
 require 'csv'
 
-CSV.foreach("input.csv", headers: true) do |row|
-  puts("#{row['Type']}: #{row['Name']}:  #{row['Currency']}\t - #{row['Amount']}  #{row['Currency']}")
-end
-
 totalBYN = 0
 totalUSD = 0
 totalEUR = 0
-table = CSV.parse(File.read("input.csv"), headers: true)
-for currency in 0..5
-  case table[currency][2].strip()
+
+CSV.foreach("input.csv", headers: true) do |row|
+  puts("#{row['Type']}: #{row['Name']}:  #{row['Currency']}\t - #{row['Amount']}  #{row['Currency']}")
+  case row['Currency']
   when "BYN"
-    totalBYN += table[currency][3].to_i
+    totalBYN += row['Amount'].to_i
   when "EUR"
-    totalUSD += table[currency][3].to_i
+    totalUSD += row['Amount'].to_i
   when "USD"
-    totalEUR += table[currency][3].to_i
+    totalEUR += row['Amount'].to_i
   end
 end
+
 puts("--------------------------------------------------")
 puts ("Total BYN"+"\t"+totalBYN.to_s)
 puts ("Total USD"+"\t"+totalUSD.to_s)
