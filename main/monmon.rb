6ff673a997b4f2d747583490f05697055371034c
Sergey <koleshkoser@gmail.com>
2020-02-23 23:41:35 +0300

    fix currency

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,22 +1,16 @@
 require 'csv'
 
-totalBYN = 0
-totalUSD = 0
-totalEUR = 0
+hash = Hash.new
 
 CSV.foreach("input.csv", headers: true) do |row|
   puts("#{row['Type']}: #{row['Name']}:  #{row['Currency']}\t - #{row['Amount']}  #{row['Currency']}")
-  case row['Currency']
-  when "BYN"
-    totalBYN += row['Amount'].to_i
-  when "EUR"
-    totalUSD += row['Amount'].to_i
-  when "USD"
-    totalEUR += row['Amount'].to_i
+  if hash.include?(row['Currency'])
+    hash[row['Currency']] += row['Amount'].to_i
+  else
+    hash[row['Currency']] = row['Amount'].to_i
   end
 end
 
-puts("--------------------------------------------------")
-puts ("Total BYN"+"\t"+totalBYN.to_s)
-puts ("Total USD"+"\t"+totalUSD.to_s)
-puts ("Total EUR"+"\t"+totalEUR.to_s)
+hash.each do |key, val|
+  puts "#{key} \t #{val}"
+end
