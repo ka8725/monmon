cb64c33639b8aeabe195be14c55fb14bcaad18cc
Sergey <koleshkoser@gmail.com>
2020-03-05 13:32:14 +0300

    fix shortcomings in issue 3

--- main/monmon.rb
+++ main/monmon.rb
@@ -2,15 +2,11 @@ require 'csv'
 
 balance = Hash.new(0)
 
-CSV.foreach("input.csv", :headers => true, :header_converters => :symbol) do |row|
+CSV.foreach('input.csv', headers: true, header_converters: :symbol) do |row|
   puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
-  if balance.include?(row[:currency].to_sym)
-    balance[row[:currency].to_sym] += row[:amount].to_i
-  else
-    balance[row[:currency].to_sym] = row[:amount].to_i
-  end
-
+  balance[row[:currency].to_sym] += row[:amount].to_i
 end
+
 puts('-----------------------------------------')
 balance.each do |key, val|
   puts "#{key} \t #{val}"
