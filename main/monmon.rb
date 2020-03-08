c82d7e150f7e73b0e3ec406c13b7dcf0e67d44a5
Sergey <koleshkoser@gmail.com>
2020-03-09 00:49:54 +0300

    Introduce list of known maintained currencies [issue 7]

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,6 +1,7 @@
 require 'csv'
 
 balance = Hash.new(0)
+support_currency = ['BYN', 'USD', 'RUR', 'EUR']
 
 CSV.foreach('input.csv', headers: true, header_converters: :symbol) do |row|
   puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
@@ -9,5 +10,9 @@ end
 
 puts('-----------------------------------------')
 balance.each do |key, val|
-  puts "#{key} \t #{val}"
+  if support_currency.include?(key.to_s)
+    puts "#{key} \t #{val}"
+  else
+    puts "#{key} \t Not supported currency!"
+  end
 end
