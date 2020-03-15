6940e379d9362cd96a57d21f9d2bbb685c36982a
Sergey <koleshkoser@gmail.com>
2020-03-16 01:44:39 +0300

    fix currency[issue 7]

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,18 +1,22 @@
 require 'csv'
 
 balance = Hash.new(0)
-support_currency = ['BYN', 'USD', 'RUR', 'EUR']
+CURRENCY = %w[BYN USD RUR EUR]
 
 CSV.foreach('input.csv', headers: true, header_converters: :symbol) do |row|
   puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
-  balance[row[:currency].to_sym] += row[:amount].to_i
+  if CURRENCY.include?(row[:currency])
+    balance[row[:currency].to_sym] += row[:amount].to_i
+  else
+    balance[row[:currency].to_sym] = "Not supported currency!"
+  end
 end
 
 puts('-----------------------------------------')
 balance.each do |key, val|
-  if support_currency.include?(key.to_s)
+  if CURRENCY.include?(key.to_s)
     puts "#{key} \t #{val}"
   else
-    puts "#{key} \t Not supported currency!"
+    puts "#{key} \t #{val}"
   end
 end
