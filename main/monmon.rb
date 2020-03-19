0a6fafaced6720eed3d3ac03c16b0cb85b08c73e
Sergey <koleshkoser@gmail.com>
2020-03-19 16:31:54 +0300

    fix names

--- main/monmon.rb
+++ main/monmon.rb
@@ -2,11 +2,11 @@ require 'csv'
 
 balance = Hash.new(0)
 not_supported = Hash.new(0)
-CURRENCY = %w[BYN USD RUR EUR]
+SUPPORTED_CURRENCIES = %w[BYN USD RUR EUR].freeze
 
 CSV.foreach('input.csv', headers: true, header_converters: :symbol) do |row|
   puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
-  if CURRENCY.include?(row[:currency])
+  if SUPPORTED_CURRENCIES.include?(row[:currency])
     balance[row[:currency].to_sym] += row[:amount].to_i
   else
     not_supported[row[:currency].to_sym] += row[:amount].to_i
