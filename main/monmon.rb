1ca569193c2cc7a2459798a55989ff37f753a931
Sergey <koleshkoser@gmail.com>
2020-03-20 18:19:16 +0300

    Make the input file name configurable

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,10 +1,10 @@
 require 'csv'
-
+params = ARGV[1]
 balance = Hash.new(0)
 not_supported = Hash.new(0)
 SUPPORTED_CURRENCIES = %w[BYN USD RUR EUR].freeze
 
-CSV.foreach('input.csv', headers: true, header_converters: :symbol) do |row|
+CSV.foreach(params, headers: true, header_converters: :symbol) do |row|
   puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
   if SUPPORTED_CURRENCIES.include?(row[:currency])
     balance[row[:currency].to_sym] += row[:amount].to_i
