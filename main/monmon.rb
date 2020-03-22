8164982a2233758a9ccedd8fe4d84625b6f2285c
Sergey <koleshkoser@gmail.com>
2020-03-23 00:22:36 +0300

    fix using other file

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,10 +1,18 @@
 require 'csv'
-params = ARGV[1]
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.on("-f", "--file") do |v|
+    options[:verbose] = v
+  end
+end.parse!
+
 balance = Hash.new(0)
 not_supported = Hash.new(0)
 SUPPORTED_CURRENCIES = %w[BYN USD RUR EUR].freeze
 
-CSV.foreach(params, headers: true, header_converters: :symbol) do |row|
+CSV.foreach(options[:verbose] == true ? ARGV[0] : "input.csv", headers: true, header_converters: :symbol) do |row|
   puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
   if SUPPORTED_CURRENCIES.include?(row[:currency])
     balance[row[:currency].to_sym] += row[:amount].to_i
