b80bf01f45bab4cd8328c9351ba38168b5ad4b1b
Sergey <koleshkoser@gmail.com>
2020-06-12 01:12:02 +0300

    add result, print, process #5

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,26 +1,38 @@
 require 'csv'
 require 'optparse'
-#require 'minitest/autorun'
-=begin
-#TEST
-#------------------------------------
-class TetsMonmon < MiniTest::Test
-  def test_totality
-    assert_equal 8540, totality(balance, total, rates, main_currency)
-  end
-end
-#------------------------------------
-=end
-def process(table, balance,not_supported)
-    table.each do |t|
 
-    if SUPPORTED_CURRENCIES.include?(t[:currency])
-        balance[t[:currency].to_sym] += t[:amount].to_i
-      else
-        not_supported[t[:currency].to_sym] += t[:amount].to_i
-      end
+# FUNCTION ----------------------
+def process(table, balance,not_supported,rates, main_currency)
+  result = Hash.new(0)
+  total = 0
+  table.each do |t|
+  if SUPPORTED_CURRENCIES.include?(t[:currency])
+      balance[t[:currency].to_sym] += t[:amount].to_i
+      result[t[:currency].to_sym] += t[:amount].to_i
+    else
+      not_supported[t[:currency].to_sym] += t[:amount].to_i
+      result[t[:currency].to_sym] = "Not supported currency!"
     end
+  end
+  balance.each do |key, val|
+    total += val * rates[key][main_currency]
+  end
+  result[:total] = total
+  return result
 end
+
+def print(result, main_currency)
+  puts('-----------------------------------------')
+  result.each do |key, val|
+    if key != :total
+      puts "#{key} \t #{val}"
+    end
+  end
+  puts('-----------------------------------------')
+  puts("Total in main currency: #{result[:total]} #{main_currency}")
+  puts('-----------------------------------------')
+end
+ #--------------------------------------------
 
 options = {file: "input.csv"}
 OptionParser.new do |opts|
@@ -35,16 +47,15 @@ OptionParser.new do |opts|
 end.parse!
 
 balance = Hash.new(0)
+result = Hash.new(0)
 not_supported = Hash.new(0)
 SUPPORTED_CURRENCIES = %w[BYN USD RUR EUR].freeze
 rates = { BYN: { USD: 0.4081, RUR: 30.1132, EUR: 0.3727 , BYN: 1 },
-        USD: { BYN: 2.4506, RUR: 73.7932, EUR: 0.9133, USD: 1},
-        RUR: { BYN: 0.0332, EUR: 0.0124, USD: 0.0136, RUR: 1 },
-        EUR: { BYN: 2.6832, RUR:80.7974, USD: 1.0949, EUR: 1 }
-        }
+USD: { BYN: 2.4506, RUR: 73.7932, EUR: 0.9133, USD: 1},
+RUR: { BYN: 0.0332, EUR: 0.0124, USD: 0.0136, RUR: 1 },
+EUR: { BYN: 2.6832, RUR:80.7974, USD: 1.0949, EUR: 1 }
+}
 main_currency = :BYN
-total = 0
-
 table = Array.new
 i = 0
 CSV.foreach(options[:file], headers: true, header_converters: :symbol) do |row|
@@ -53,19 +64,5 @@ CSV.foreach(options[:file], headers: true, header_converters: :symbol) do |row|
     i = i+1
 end
 
-process(table, balance,not_supported)
-
-puts('-----------------------------------------')
-balance.each do |key, val|
-  puts "#{key} \t #{val}"
-end
-not_supported.each do |key, val|
-  puts "#{key} \t Not supported currency!"
-end
-
-balance.each do |key, val|
-  total += val * rates[key][main_currency]
-end
-
-puts('-----------------------------------------')
-puts("Total in main currency: #{total} #{main_currency}")
+result = process(table, balance,not_supported,rates,main_currency)
+print(result, main_currency)
