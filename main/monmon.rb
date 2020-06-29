19047c362007a5969bfe27f4c20e15e46c00c491
Sergey <koleshkoser@gmail.com>
2020-06-29 23:12:18 +0300

    add test.rb #5

--- main/monmon.rb
+++ main/monmon.rb
@@ -1,33 +1,7 @@
 require 'csv'
 require 'optparse'
 require 'minitest/autorun'
-
-#TEST
-#------------------------------------
-class TetsMonmon < MiniTest::Test
-  def test_process
-    table = [{:type=>"CC", :name=>"Alfa-bank CC", :currency=>"BYN", :amount=>"500"},
-    {:type=>"CC", :name=>"MyBank CC", :currency=>"USD", :amount=>"600"},
-    {:type=>"Cash", :name=>"In the car", :currency=>"EUR", :amount=>"200"},
-    {:type=>"Cash", :name=>"At home", :currency=>"RUR", :amount=>"1000"},
-    {:type=>"Cash", :name=>"At home", :currency=>"UAH", :amount=>"1000"},
-    {:type=>"Bank account", :name=>"Paritet Bank", :currency=>"BYN", :amount=>"2000"},
-    {:type=>"Deposit account", :name=>"Priorbank", :currency=>"BYN", :amount=>"4000"}]
-
-    rates =  { BYN: { USD: 0.4081, RUR: 30.1132, EUR: 0.3727 , BYN: 1 },
-      USD: { BYN: 2.4506, RUR: 73.7932, EUR: 0.9133, USD: 1 },
-      RUR: { BYN: 0.0332, EUR: 0.0124, USD: 0.0136, RUR: 1 },
-      EUR: { BYN: 2.6832, RUR:80.7974, USD: 1.0949, EUR: 1 }}
-
-    main_currency = :BYN
-    not_supported = {:UAH=>1000}
-    balance = {:BYN=>6500, :USD=>600, :EUR=>200, :RUR=>1000}
-
-    result =  process(table, balance,not_supported,rates, main_currency)
-    asert_equal 6500, result[:BYN]
-  end
-end
-#------------------------------------
+load 'test.rb'
 
 # FUNCTION --------------------------
 def process(table, balance,not_supported,rates, main_currency)
@@ -61,7 +35,6 @@ def print(result, main_currency)
   puts('-----------------------------------------')
 end
  #------------------------------------
-
 
 options = {file: "input.csv"}
 OptionParser.new do |opts|
