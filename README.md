f2fe63089c322552254f66a261e32eabbf82c0b8
Sergey <koleshkoser@gmail.com>
2020-10-13 01:07:54 +0300

    fix problem 2 #21

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # monmon
 
 Run tests: `ruby -I lib test/process_test.rb`
-Ruby CLI: `./bin/cli -a DB/CSV`
+Ruby CLI: `./bin/cli -a DB` to run with database
+          `./bin/cli -a CSV` to run with CSV file
--- lib/cli.rb
+++ lib/cli.rb
@@ -6,31 +6,30 @@ def convert_keys(table)
 end
 
 main_currency = :BYN
-table = Array.new
+table = []
 
 options = {file: 'input.csv'}
 OptionParser.new do |opts|
-  opts.banner = "Monmon can calculate the sum of all income and a certain currency.\nUsage:   monmon.rb [options]:"
-  opts.on("-f name", "--file=name", "Input file in CSV format") do |v|
+  opts.banner = 'Monmon can calculate the sum of all income and a certain currency.'+"\n"+'Usage:   monmon.rb [options]:'
+  opts.on('-f name', '--file=name', 'Input file in CSV format') do |v|
     options[:file] = v
   end
-  opts.on("-h", "--help", "Prints this help") do
+  opts.on('-h', '--help', 'Prints this help') do
     puts opts
     exit
   end
-  opts.on("-a name", "--adapter=name", "Run with datadase or CSV") do |v|
-    if v == "DB"
+  opts.on('-a name', '--adapter=name', 'Run with datadase or CSV') do |v|
+    if v == 'DB'
       conn = PG::Connection.open( dbname: 'monmon', user: 'postgres')
-      table = convert_keys(conn.exec("SELECT * from accounts"))
-    elsif v == "CSV"
+      table = convert_keys(conn.exec('SELECT * from accounts'))
+    elsif v == 'CSV'
       i = 0
       CSV.foreach(options[:file], headers: true, header_converters: :symbol) do |row|
-        puts("#{row[:type]}: #{row[:name]}:  #{row[:currency]}\t - #{row[:amount]}  #{row[:currency]}")
         table[i] = {type: row[:type], name: row[:name], currency: row[:currency], amount: row[:amount]}
         i = i+1
       end
     else
-      abort "Error!"
+      abort 'Parameter usage error!'+"\n"+'Use "./bin/cli -a DB" to run with database'+"\n"+'Use "./bin/cli -a CSV" to run with CSV file'
     end
   end
 end.parse!
