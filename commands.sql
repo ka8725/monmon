f7e6e5ab9cab25d6a244ac2880c30fdbee1fa1f3
Sergey <koleshkoser@gmail.com>
2020-11-18 22:55:52 +0300

    Introduced an ENV var that allows to specify connection URL to pg

---
+++ commands.sql
@@ -0,0 +1,11 @@
+create database monmon;
+create table accounts(Type character varying(15),Name character varying(25),Currency character varying(4),Amount integer);
+insert into accounts(Type,Name,Currency,Amount)
+  values ('CC','Alfa-bank CC','BYN',500),
+  ('CC','MyBank CC','USD',600),
+  ('Cash','In the car','EUR',200),
+  ('Cash','At home','RUB',1000),
+  ('Cash','At home','UAH',1000),
+  ('Bank account','Paritet Bank','BYN',2000),
+  ('Deposit account','Priorbank','BYN',4000);
+select * from accounts;
